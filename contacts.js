a1326877f0926da16d2f28bcff72eabf60039770
talask <natasha169@ukr.net>
2023-08-03 21:33:39 +0300

    update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid';
 const fs = require('fs').promises;
 const path = require('path');
 
@@ -8,17 +9,16 @@ const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 // TODO: задокументувати кожну функцію
 function listContacts() {
     fs.readFile(contactsPath)
-  .then(
-    data => {
-      const contacts = JSON.parse(data);
+    .then(
+      data => {
+        const contacts = JSON.parse(data);
 
-      console.log("   Name".padEnd(20, ' '), "   Email".padEnd(50, ' '), "   Phone".padEnd(15, ' '));
+        console.log("   Name".padEnd(20, ' '), "   Email".padEnd(50, ' '), "   Phone".padEnd(15, ' '));
 
-      contacts.forEach(({name, email, phone}) => {
-        console.log(name.padEnd(20, ' '), email.padEnd(50, ' ') , phone.padEnd(15, ' ') );  
+        contacts.forEach(({name, email, phone}) => {
+          console.log(name.padEnd(20, ' '), email.padEnd(50, ' ') , phone.padEnd(15, ' ') );  
       });
-    }
-    )
+    })
   .catch(err => console.log(err.message));
   }
  // listContacts();
@@ -37,12 +37,55 @@ function listContacts() {
     )
   .catch(err => console.log(err.message));
   }
-  getContactById("rsKkOQUi80UsgVPCcLZZW");
+  //getContactById("rsKkOQUi80UsgVPCcLZZW");
   
-//   function removeContact(contactId) {
-//     // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-//   }
+  function removeContact(contactId) {
+    fs.readFile(contactsPath)
+  .then(
+    data => {
+      const contacts = JSON.parse(data);
+
+      console.log("   Name".padEnd(20, ' '), "   Email".padEnd(50, ' '), "   Phone".padEnd(15, ' '));
+
+      const findContact = contacts.find(({id}) =>  id === contactId );
+
+      console.log(findContact ? `${findContact.name.padEnd(20, ' ')} ${findContact.email.padEnd(50, ' ')} ${findContact.phone.padEnd(15, ' ')}` : null);
+
+      if (findContact) {
+
+        const filterContact = contacts.find(({id}) =>  id !== contactId );
+
+        fs.writeFile(contactsPath, JSON.stringify(filterContact))
+        .then( data => {
+          console.log("data");
+          console.log(data);
+        })
+        .catch(err => console.log(err.message));
+
+      }
+      })
+  .catch(err => console.log(err.message));
+  }
   
-//   function addContact(name, email, phone) {
-//     // ...твій код. Повертає об'єкт доданого контакту. 
-//   }
+  removeContact("rsKkOQUi80UsgVPCcLZZW1");
+
+  function addContact(name, email, phone) {
+    
+    const newObj = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    const jsonData = JSON.stringify(newObj);
+
+    fs.appendFile(contactsPath, jsonData)
+    .then( data => {
+      console.log("data");
+      console.log(data);
+    })
+    .catch(err => console.log(err.message));
+
+  }
+
+  addContact("Rose Rose", "roserose@ukr.net", "(678)657-67-95")
